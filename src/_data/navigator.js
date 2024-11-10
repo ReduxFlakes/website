4c11a4ac153f893f8289def57309c15b2bbcf197
ReduxFlakes <tiagompca@proton.me>
2024-11-10 23:16:49 +0000

    changes

---
+++ src/_data/navigator.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    title: "home",
+    url: "",
+  },
+  {
+    title: "posts",
+    url: "",
+  },
+  {
+    title: "bookmarks",
+    url: "",
+  },
+  {
+    title: "projects",
+    url: "",
+  },
+  {
+    title: "shrines",
+    url: "",
+  },
+  {
+    title: "design",
+    url: "",
+  },
+  {
+    title: "now",
+    url: "",
+  },
+  {
+    title: "uses",
+    url: "",
+  },
+];
