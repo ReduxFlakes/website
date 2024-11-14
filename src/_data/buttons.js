a26df5161d9e4ebb14d917a46ae6d368feb95203
ReduxFlakes <tiagompca@proton.me>
2024-11-14 23:32:40 +0000

    nice design tweaks and new features!!

---
+++ src/_data/buttons.js
@@ -0,0 +1,34 @@
+export default {
+  marquee: [
+    {
+      title: "Melankorin",
+      img: "melankorin.gif",
+      url: "https://melankorin.net/",
+    },
+    {
+      title: "june's web page",
+      img: "webcatz.png",
+      url: "https://webcatz.neocities.org/",
+    },
+    {
+      title: "Levant's Cyberspace",
+      img: "levant.gif",
+      url: "https://levant.neocities.org/",
+    },
+    {
+      title: "dimden",
+      img: "dimden.gif",
+      url: "https://dimden.dev/",
+    },
+    {
+      title: "MystSaphyr",
+      img: "mystsaphyr.gif",
+      url: "https://mystsaphyr.art/",
+    },
+    {
+      title: "Shishka",
+      img: "shishka.gif",
+      url: "https://shishka.neocities.org/",
+    },
+  ],
+};
