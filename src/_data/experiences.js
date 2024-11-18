726c0701b4a4f629caa10870f5ff22f3c8328fe2
ReduxFlakes <87225951+ReduxFlakes@users.noreply.github.com>
2024-11-18 11:17:29 +0000

    added new section and some css tweaks

---
+++ src/_data/experiences.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    title: "The Mall",
+    url: "/expo/mall",
+    img: "the_mall.png",
+  },
+  {
+    title: "SurfScape",
+    url: "/expo/surfscape",
+    img: "",
+  },
+  {
+    title: "CompuSoft",
+    url: "/expo/compusoft",
+    img: "",
+  },
+  {
+    title: "CompuSoft",
+    url: "/expo/compusoft",
+    img: "",
+  },
+];
