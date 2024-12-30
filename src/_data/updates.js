408cfa4d38ad00dfad130e889b1d475a72c321a5
ReduxFlakes <tiagompca@proton.me>
2024-12-30 18:34:15 +0000

    add: deploy scripts and changed last update date

--- src/_data/updates.js
+++ src/_data/updates.js
@@ -1,6 +1,6 @@
 export default [
   {
-    updateDate: "2024-12-28",
+    updateDate: "2024-12-30",
     content:
       "I've finally released the new website! Now available on Neocities and Nekoweb. More content coming soon (i hope).",
   },
