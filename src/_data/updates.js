c842c85f4d37ed34a94050fd228b2e32e154df83
ReduxFlakes <tiagompca@proton.me>
2025-01-04 03:05:32 +0000

    add: changelog for 4 January 2025

--- src/_data/updates.js
+++ src/_data/updates.js
@@ -1,5 +1,10 @@
 export default [
   {
+    updateDate: "2025-01-04",
+    content:
+      "Added new buttons, fixed the navbar on mobile and other tweaks on the CSS, fixed broken links and types and other stuff.",
+  },
+  {
     updateDate: "2024-12-30",
     content:
       "I've finally released the new website! Now available on Neocities and Nekoweb. More content coming soon (i hope).",
