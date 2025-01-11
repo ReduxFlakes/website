3d97422f8f507197890eb4175eea58bb1b626443
ReduxFlakes <tiagompca@proton.me>
2025-01-11 19:27:50 +0000

    add changelog for 11 January 2025

--- src/_data/updates.js
+++ src/_data/updates.js
@@ -1,5 +1,10 @@
 export default [
   {
+    updateDate: "2025-01-11",
+    content:
+      "Fixed the navbar again on mobile, added new projects and added feed icons on the blog page.",
+  },
+  {
     updateDate: "2025-01-04",
     content:
       "Added new buttons, fixed the navbar on mobile and other tweaks on the CSS, fixed broken links and types and other stuff.",
