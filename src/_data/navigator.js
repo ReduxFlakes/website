a0db98ccb98b2790740b5827c3cef42c1728a223
ReduxFlakes <87225951+ReduxFlakes@users.noreply.github.com>
2025-02-06 17:36:06 +0000

    removed junk

--- src/_data/navigator.js
+++ src/_data/navigator.js
@@ -1,19 +1,11 @@
 export default [
   {
-    title: "home",
-    url: "/",
-  },
-  {
     title: "blog",
     url: "/blog/",
   },
   {
     title: "projects",
     url: "/projects/",
-  },
-  {
-    title: "extras",
-    url: "/extras/",
   },
   {
     title: "about",
