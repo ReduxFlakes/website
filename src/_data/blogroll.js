eb77dca88bb1075470be36baf25b879abe08c963
ReduxFlakes <tiagompca@proton.me>
2025-08-14 09:48:04 +0100

    added new entries to the blogroll

--- src/_data/blogroll.js
+++ src/_data/blogroll.js
@@ -9,6 +9,36 @@ export default [
         title: "FlamedFury",
         feed: "https://flamedfury.com/feed.xml",
         url: "https://flamedfury.com/",
-        description: "Life, music, and the personal web"
+        description: "Life, music and the personal web"
+    },
+    {
+        title: "Seirdy",
+        feed: "https://seirdy.one/posts/atom.xml",
+        url: "https://seirdy.one/",
+        description: "Tech, privacy, AI, programming"
+    },
+    {
+        title: "lime360",
+        feed: "https://blog.yoyle.city/feed/",
+        url: "https://yoyle.city/",
+        description: "State of the internet and website updates"
+    },
+    {
+        title: "jbcarreon123",
+        feed: "https://jbc.lol/feed.xml",
+        url: "https://jbc.lol/",
+        description: "The web, life and devlogs"
+    },
+    {
+        title: "CozyNet",
+        feed: "https://www.cozynet.org/feed/feed.xml",
+        url: "https://www.cozynet.org/",
+        description: "Life, Trinity Desktop adventures and site updates"
+    },
+    {
+        title: "Daudix (Progressive Enhancement)",
+        feed: "https://daudix.one/blog/atom.xml",
+        url: "https://daudix.one/",
+        description: "Personal projects, life and devlogs"
     }
 ]
