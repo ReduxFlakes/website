e6a91dae35734a77316623665193720106b9b7fb
ReduxFlakes <tiagompca@proton.me>
2025-10-04 23:32:32 +0100

    removed FEX remasters after DMCA

--- src/_data/downloads/remasters.js
+++ src/_data/downloads/remasters.js
@@ -7,7 +7,7 @@ export default [
         format: "FLAC",
         youtube: "https://youtu.be/RA5FzMO6wx8",
         sources: [
-        "https://file.garden/aAEXMwgY3QmuIjxE/Remasters/Johnny%20In%20The%20Middle%20Of%20The%20Park.flac"
+            "https://file.garden/aAEXMwgY3QmuIjxE/Remasters/Johnny%20In%20The%20Middle%20Of%20The%20Park.flac"
         ]
     },
     {
@@ -17,21 +17,10 @@ export default [
         format: "FLAC",
         youtube: "https://youtu.be/qU9XX1QvhX4",
         sources: [
-        "https://file.garden/aAEXMwgY3QmuIjxE/Remasters/The%20Burns/The%20Burns%20-%20Avail"
+            "https://file.garden/aAEXMwgY3QmuIjxE/Remasters/The%20Burns/The%20Burns%20-%20Avail"
         ]
     },
-    {
-        title: "FEX - Subways Of Your Mind",
-        description: "This is the enhanced version I did after receiving some comments hating the YT remaster.",
-        time: "3:44",
-        size: "39,4 MiB",
-        format: "WAV",
-        youtube: "https://youtu.be/aWs8xvCZXwA",
-        sources: [
-        "https://file.garden/aAEXMwgY3QmuIjxE/Remasters/FEX/Subways%20Of%20Your%20Mind.wav",
-        "https://mega.nz/file/4FZ0XbwQ#vmqEe7pe_mo6azTbHzHImyAZwkglttfUZpQFt56n5VY"
-        ]
-    },
+
     {
         title: "3D (a fish in sea) - Dance To Believe",
         time: "4:36",
@@ -39,7 +28,7 @@ export default [
         format: "M4A (AAC)",
         youtube: "https://youtu.be/CJVsj7KyLDo",
         sources: [
-        "https://file.garden/aAEXMwgY3QmuIjxE/Remasters/3D%20Fish%20In%20Sea/Dance%20to%20Believe.m4a",
+            "https://file.garden/aAEXMwgY3QmuIjxE/Remasters/3D%20Fish%20In%20Sea/Dance%20to%20Believe.m4a",
         ]
     },
     {
@@ -49,7 +38,7 @@ export default [
         format: "M4A (AAC)",
         youtube: "https://youtu.be/tmLFoXtl5Us",
         sources: [
-        "https://file.garden/aAEXMwgY3QmuIjxE/Remasters/3D%20Fish%20In%20Sea/Nearer.m4a",
+            "https://file.garden/aAEXMwgY3QmuIjxE/Remasters/3D%20Fish%20In%20Sea/Nearer.m4a",
         ]
     },
     {
@@ -59,7 +48,7 @@ export default [
         format: "M4A (AAC)",
         youtube: "https://youtu.be/hilh4Tm8voM",
         sources: [
-        "https://file.garden/aAEXMwgY3QmuIjxE/Remasters/3D%20Fish%20In%20Sea/The%20Club.m4a",
+            "https://file.garden/aAEXMwgY3QmuIjxE/Remasters/3D%20Fish%20In%20Sea/The%20Club.m4a",
         ]
     },
     {
@@ -69,7 +58,7 @@ export default [
         format: "FLAC",
         youtube: "https://youtu.be/dJwfeHI15oE",
         sources: [
-        "https://file.garden/aAEXMwgY3QmuIjxE/Remasters/The%20Burns/The%20Burns%20-%20Sidewalks.flac",
+            "https://file.garden/aAEXMwgY3QmuIjxE/Remasters/The%20Burns/The%20Burns%20-%20Sidewalks.flac",
         ]
     }
 ]
