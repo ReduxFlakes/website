f40649a02b487aac2f173e929cd62790eae79c33
ReduxFlakes <87225951+ReduxFlakes@users.noreply.github.com>
2025-12-31 19:38:14 +0000

    changelog for December 31

--- src/_data/updates.js
+++ src/_data/updates.js
@@ -1,9 +1,16 @@
 export default [
   {
+    date: "2025-12-32 19:37:10",
+    content: "Happy New Year!!",
+    list: [
+      "Added new button to the friends section",
+      "Remove wrong line in the previous update"
+    ]
+  },
+  {
     date: "2025-12-21 19:34:21",
     content: "Some updates and new blog post!",
     list: [
-      "Published a new post about my move to Linux and how my experience has been so far",
       "Updates to the gaming page (+ new section)",
       "Updates to the projects page with some fixes, a new idea, and a new project i'm involved in",
       "Tweaked the default theme once again",
