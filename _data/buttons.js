8764bb2876f22dbf1da13e0506e9df99738e6ad9
ReduxFlakes <87225951+ReduxFlakes@users.noreply.github.com>
2026-02-01 01:40:20 +0000

    updated buttons

---
+++ _data/buttons.js
@@ -0,0 +1,730 @@
+export default {
+  friends: [
+    {
+      title: "LRS",
+      img: "LRS.gif",
+      url: "https://lastroseofsummer.neocities.org",
+    },
+    {
+      title: "ByteMoth",
+      img: "bytemoth.png",
+      url: "https://bytemoth.neocities.org/",
+    },
+    {
+      title: "immjs",
+      img: "https://immjs.dev/88x31s/immjs.dev.gif",
+      externalimg: true,
+      url: "https://immjs.dev/",
+    },
+    {
+      title: "Xan",
+      img: "xan.gif",
+      url: "https://xan.lol//",
+    },
+    {
+      title: "Jak2K",
+      img: "jak2k.webp",
+      url: "https://jak2k.eu/",
+    },
+    {
+      title: "Daudix",
+      img: "daudix.gif",
+      url: "https://daudix.one/",
+    },
+    {
+      title: "iox",
+      img: "https://ioxtv.co/assets/buttons/iox.gif",
+      externalimg: true,
+      url: "https://ioxtv.co/"
+    },
+    {
+      title: "Thin Liquid",
+      img: "thnlqd.png",
+      url: "https://thinliquid.dev/",
+    },
+    {
+      title: "max's apartment",
+      img: "maxs.gif",
+      url: "https://maxpixels.moe/",
+    },
+    {
+      title: "jbcarreon123",
+      img: "jbcarreon123.png",
+      url: "https://jbc.lol/",
+    },
+    {
+      title: "OpenBooks",
+      img: "openbooks.gif",
+      url: "https://openbooks.neocities.org",
+    },
+    {
+      title: "Badgraph1csghost",
+      img: "badgraph1csghost.png",
+      url: "https://badgraph1csghost.neocities.org/",
+    },
+    {
+      title: "joosh's Homepage!",
+      img: "jooshRice.gif",
+      url: "https://joo.sh/",
+    },
+    {
+      title: "fLaMEd fury",
+      img: "fury.gif",
+      url: "https://flamedfury.com/",
+    },
+    {
+      title: "rice.place",
+      img: "riceplace.png",
+      url: "https://rice.place/",
+    },
+    {
+      title: "Velvet Blue",
+      img: "velvetblue.png",
+      url: "https://velvetblue.neocities.org",
+    },
+    {
+      title: "Daniele's Website",
+      img: "danieles.jpg",
+      url: "https://daniele63.com/",
+    },
+    {
+      title: "Fabled",
+      img: "fabled.gif",
+      url: "https://fabled.day",
+    },
+    {
+      title: "Aristasian Reminiscence",
+      img: "aristasiabutton.gif",
+      url: "https://aristasia.guide/",
+    },
+    {
+      title: "Frutiger Aero Archive",
+      img: "frutigeraeroarchive.png",
+      url: "https://frutigeraeroarchive.org/",
+    },
+    {
+      title: "Leilukin's Hub",
+      img: "leilukins-hub.png",
+      url: "https://leilukin.neocities.org",
+    },
+    {
+      title: "july.lol",
+      img: "lonelyjuly.gif",
+      url: "https://july.lol/",
+    },
+    {
+      title: "Wlodekm",
+      img: "wlodekm.png",
+      url: "https://wlodekm.nekoweb.org/",
+    },
+    {
+      title: "Yudosai",
+      img: "yudosai.png",
+      url: "https://yudo.cc/",
+    },
+    {
+      title: "Slushienova",
+      img: "slush.jpg",
+      url: "https://slushienova.neocities.org/",
+    },
+    {
+      title: "Houdini",
+      img: "houdininokia.gif",
+      url: "https://erikhoudini.com/",
+    },
+    {
+      title: "Bulltown.neo",
+      img: "bulltown.png",
+      url: "https://bulltown.neocities.org",
+    },
+    {
+      title: "Hifumi's Weebsite",
+      img: "lhfm.png",
+      url: "https://lhfm.neocities.org/",
+    },
+    {
+      title: "NorthSilver",
+      img: "northsilver.png",
+      url: "https://northsilversite.neocities.org/",
+    },
+    {
+      title: "Fan0102",
+      img: "fan0102.gif",
+      url: "https://somefan0102.neocities.org/",
+    },
+    {
+      title: "Coloured",
+      img: "conline.png",
+      url: "https://coloured.neocities.org/",
+    },
+    {
+      title: "Lux Fero",
+      img: "lux_fero.gif",
+      url: "https://luxfero.su/en/",
+    },
+    {
+      title: "NomnomNami",
+      img: "nomnomnami.gif",
+      url: "https://nomnomnami.com/",
+    },
+    {
+      title: "yatagarasu",
+      img: "yatagarasu.gif",
+      url: "https://yatagarasu.nekoweb.org/",
+    },
+    {
+      title: "visualculture",
+      img: "visualculture.png",
+      url: "https://visualculture.vc/",
+    },
+    {
+      title: "dabric",
+      img: "dabric.png",
+      url: "https://dabric.xyz/",
+    },
+    {
+      title: "GrayBox",
+      img: "graybox.gif",
+      url: "https://graybox.lol/",
+    },
+    {
+      title: "concon.sou",
+      img: "concon.gif",
+      url: "https://concon.soy/",
+    },
+    {
+      title: "tfpXE",
+      img: "tfpxe.gif",
+      url: "https://tfpxe.wtf/",
+    },
+    {
+      title: "soulwebb",
+      img: "soulwebb.gif",
+      url: "https://soulwebb.ing/",
+    },
+    {
+      title: "ui(ooo-eee)",
+      img: "oooeee.png",
+      url: "https://oooeee.nekoweb.org/",
+    },
+    {
+      title: "Layercake",
+      img: "layercake.gif",
+      url: "https://layercake.nekoweb.org/",
+    },
+    {
+      title: "PixelGlade",
+      img: "pixelglade.png",
+      url: "https://pixelglade.net.au/",
+    },
+    {
+      title: "FOGGYBEAR42",
+      img: "foggybear42.gif",
+      url: "https://foggybear42.net/",
+    },
+    {
+      title: "Atari404",
+      img: "atari404.gif",
+      url: "https://atari404.neocities.org/",
+    },
+    {
+      title: "ShinyEXE",
+      img: "shinyexe.gif",
+      url: "https://shinyexe.neocities.org/",
+    },
+    {
+      title: "Superbug",
+      img: "superbug.gif",
+      url: "https://superbug.neocities.org/",
+    },
+    {
+      title: "Pinkderg",
+      img: "pinkderg.gif",
+      url: "https://pinkderg.art/",
+    },
+    {
+      title: "ELEVATOR 2 THE MOON",
+      img: "elevatorttm.png",
+      url: "https://elevator-to-the-moon.neocities.org/",
+    },
+    {
+      title: "HELLSTAR ARES",
+      img: "hellstarares.gif",
+      url: "https://hellstarares.rip/",
+    },
+    {
+      title: "The yeepsite",
+      img: "yeepsite.webp",
+      url: "https://yeepsite.nekoweb.org/",
+    },
+    {
+      title: "uksz hub",
+      img: "uksz.gif",
+      url: "https://uksz.org/",
+    },
+    {
+      title: "Dannarchy",
+      img: "dannarchy.gif",
+      url: "https://dannarchy.com",
+    },
+    {
+      title: "NeoCities",
+      img: "neo-neighborhoods.gif",
+      url: "https://neo-neighborhoods.neocities.org/",
+    },
+    {
+      title: "Tapeykatt's Place",
+      img: "tapeykatt.gif",
+      url: "https://tapeykatt.neocities.org/",
+    },
+    {
+      title: "Avedo's Line n Sinker",
+      img: "avedo.png",
+      url: "https://avedo-storyhub.neocities.org/",
+    },
+    {
+      title: "Systemic Cyclops",
+      img: "cycl.gif",
+      url: "https://systemiccyclops.neocities.org/",
+    },
+    {
+      title: "pirahxcx",
+      img: "pirahxcx.gif",
+      url: "https://pirahxcx.neocities.org/"
+    },
+    {
+      title: "isobel's web",
+      img: "isobelsweb.gif",
+      url: "https://isobelsweb.com/"
+    },
+    {
+      title: "xolimni's HQ",
+      img: "xsoliminishq.gif",
+      url: "https://xsolimini.fr/"
+    },
+    {
+      title: "Knoxie's World",
+      img: "knoxstation.gif",
+      url: "https://knoxstation.neocities.org/"
+    },
+    {
+      title: "bielzin's space",
+      img: "bielzin.png",
+      url: "https://bielzin.space/"
+    },
+    {
+      title: "Kanan draw",
+      img: "kanan.gif",
+      url: "https://kanandraw.neocities.org/"
+    },
+    {
+      title: "Demgoze",
+      img: "demgoze.png",
+      url: "https://demgoze.nekoweb.org/",
+    },
+    {
+      title: "madoxy",
+      img: "madoxy.gif",
+      url: "https://madoxy.neocities.org/",
+    },
+    {
+      title: "splashy",
+      img: "splashy.png",
+      url: "https://splashy.neocities.org/",
+    },
+    {
+      title: "KWTC",
+      img: "kwtc.gif",
+      url: "https://kidwiththechemicalz.net/",
+    },
+    {
+      title: "VIVISECTION",
+      img: "vivisection.gif",
+      url: "https://v1v1sect1on.nekoweb.org/"
+    },
+    {
+      title: "aid's small corner",
+      img: "zhongvie.png",
+      url: "https://zhongvie.neocities.org/"
+    },
+    {
+      title: "Underwhite",
+      img: "underwhite.gif",
+      url: "https://underwhite.neocities.org/"
+    },
+
+    {
+      title: "Greenbox",
+      img: "greenbox.png",
+      url: "https://greenbox.network/"
+    },
+    {
+      title: "philia995",
+      img: "philia995.gif",
+      url: "https://philia995.neocities.org/"
+    },
+    {
+      title: "da ditzy dmns!",
+      img: "ditzydmns.gif",
+      url: "https://ditzydmns.nekoweb.org/"
+    },
+    {
+      title: "dabric",
+      img: "https://dabric.xyz/butt.png",
+      externalimg: true,
+      url: "https://dabric.xyz/"
+    },
+    {
+      title: "mars",
+      img: "mars.gif",
+      url: "https://mars.nekoweb.org/"
+    },
+    {
+      title: "dreamscape",
+      img: "dreamscape.png",
+      url: "https://dreamscape.place/"
+    },
+    {
+      title: "moru's pond",
+      img: "moru.png",
+      url: "https://morushroom.nekoweb.org/"
+    },
+    {
+      title: "Yume Kuni",
+      img: "yumekuni.png",
+      url: "https://yumekuni.moe/"
+    },
+    {
+      title: "nikonet",
+      img: "nikonet.gif",
+      url: "https://nikonet.xyz/"
+    },
+    {
+      title: "eepynoelles website :D",
+      img: "eepynoelle.png",
+      url: "https://eepynoelle.nekoweb.org/"
+    },
+    {
+      title: "gwenbnuuy",
+      img: "https://gwenbnuuy.moe/images/buttons/sitebutton.gif",
+      externalimg: true,
+      url: "https://gwenbnuuy.moe/"
+    },
+    {
+      title: "sunnytheorem",
+      img: "https://sunnytheorem.nekoweb.org/buttons/sunnytheorembutton.gif",
+      externalimg: true,
+      url: "https://sunnytheorem.nekoweb.org/"
+    },
+    {
+      title: "Cyncrovee",
+      url: "https://cyncrovee.nekoweb.org/"
+    },
+    {
+      title: "july.lol",
+      url: "https://july.lol/"
+    },
+    {
+      title: "Lukas Zone",
+      url: "https://lukaszone.neocities.org/",
+    },
+    {
+      title: "scuti",
+      url: "https://scuti.neocities.org/",
+    },
+    {
+      title: "Miko",
+      url: "https://miko.nekoweb.org/",
+    },
+    {
+      title: "Flux Veritas",
+      url: "https://belarius.neocities.org/",
+    },
+    {
+      title: "xandra",
+      url: "https://xandra.cc/",
+    },
+    {
+      title: "danielStrawb",
+      url: "https://danielsblog.neocities.org/",
+    },
+    {
+      title: "lehr",
+      url: "https://jlehr.neocities.org/",
+    },
+    {
+      title: "nolove",
+      url: "https://nolove.neocities.org/",
+    },
+    {
+      title: "Caesár",
+      url: "https://caesar.wtf/",
+    },
+    {
+      title: "juice",
+      url: "https://firebaseripcord.neocities.org/"
+    },
+  ],
+  likes: [
+    {
+      title: "32bit cafe",
+      img: "32bitty.png",
+      url: "https://32bit.cafe/",
+    },
+    {
+      title: "Melonking",
+      img: "melonking.gif",
+      url: "https://melonking.net/",
+    },
+    {
+      title: "Absolute Realm",
+      img: "absolute-realm.gif",
+      url: "https://theabsoluterealm.com/",
+    },
+    {
+      title: "bomberfish.ca",
+      img: "bfca.gif",
+      url: "https://bomberfish.ca/",
+    },
+    {
+      title: "Melankorin",
+      img: "melankorin.gif",
+      url: "https://melankorin.net/",
+    },
+    {
+      title: "CozyNet",
+      img: "cozybro88.gif",
+      url: "https://www.cozynet.org/",
+    },
+    {
+      title: "Freeplay",
+      img: "free.gif",
+      url: "https://freeplay.floof.company/",
+    },
+    {
+      title: "pjfrix",
+      img: "frix.gif",
+      url: "https://park-city.club/~frix/",
+    },
+    {
+      title: "dimden",
+      img: "dimden.gif",
+      url: "https://dimden.dev/",
+    },
+    {
+      title: "HykicNova",
+      img: "hykicnova.png",
+      url: "https://nova.nekoweb.org/",
+    },
+    {
+      title: "june's web page",
+      img: "webcatz.png",
+      url: "https://webcatz.neocities.org/",
+    },
+    {
+      title: "datguy",
+      img: "datguy.gif",
+      url: "https://datguy.neocities.org/",
+    },
+    {
+      title: "Seirdy's Home",
+      img: "seirdy.gif",
+      url: "https://seirdy.one/",
+    },
+    {
+      title: "Shishka",
+      img: "shishka.gif",
+      url: "https://shishka.neocities.org/",
+    },
+    {
+      title: "rm94",
+      img: "rm94.gif",
+      url: "https://rm94.neocities.org/",
+    },
+    {
+      title: "Pixel",
+      img: "pixeldesu.png",
+      url: "https://pixelde.su/",
+    },
+    {
+      title: "Solinu's Website",
+      img: "solinus.gif",
+      url: "https://solinus.neocities.org/",
+    },
+    {
+      title: "meows.zip",
+      img: "meowsdotzip.png",
+      url: "https://meows.zip/",
+    },
+    {
+      title: "Shellsharks",
+      img: "shellsharks.gif",
+      url: "https://shellsharks.com/",
+    },
+    {
+      title: "Levant's Cyberspace",
+      img: "levant.gif",
+      url: "https://levant.neocities.org/",
+    },
+    {
+      title: "MystSaphyr",
+      img: "mystsaphyr.gif",
+      url: "https://mystsaphyr.art/",
+    },
+    {
+      title: "mux",
+      img: "mux.png",
+      url: "https://mux.nekoweb.org/",
+    },
+    {
+      title: "remblanc",
+      img: "remblanc.png",
+      url: "https://remblanc.com/",
+    },
+    {
+      title: "nuthead",
+      img: "nuthead.gif",
+      url: "https://nuthead.neocities.org/",
+    },
+    {
+      title: "sizeof(cat)",
+      img: "sizeofcat.jpg",
+      url: "https://sizeof.cat/",
+    },
+    {
+      title: "RoBu",
+      img: "rbuchanan.png",
+      url: "https://rbuchanan.neocities.org/",
+    },
+    {
+      title: "2Bit",
+      img: "2bit.png",
+      url: "https://2bit.neocities.org/",
+    },
+    {
+      title: "newchapter.org",
+      img: "newchapter.gif",
+      url: "https://newchapter.neocities.org",
+    },
+    {
+      title: "Koinuko",
+      img: "koinuko.gif",
+      url: "https://koinuko.pink/",
+    },
+    {
+      title: "Bootleg64",
+      img: "bl64.png",
+      url: "https://bootleg64.neocities.org/",
+    },
+    {
+      title: "TEOMODO",
+      img: "teomodo.gif",
+      url: "https://teomodo.net/",
+    },
+    {
+      title: "Cyuucat",
+      img: "cyuu.gif",
+      url: "https://cyuucat.moe/",
+    },
+    {
+      title: "lempamo",
+      img: "lempamo.png",
+      url: "https://lempamo.glitch.me/",
+    },
+    {
+      title: "elke.cafe",
+      img: "elkecafe.gif",
+      url: "https://elke.cafe/",
+    },
+    {
+      title: "ari melody",
+      img: "arimelody.gif",
+      url: "https://arimelody.space/",
+    },
+    {
+      title: "Lars",
+      img: "lars.png",
+      url: "https://larsfrommars.neocities.org/",
+    },
+    {
+      title: "sneexy",
+      img: "sneexy.svg",
+      url: "https://sneexy.pages.gay/",
+    },
+    {
+      title: "olivia",
+      img: "olivia.png",
+      url: "https://floof.gay/",
+    },
+    {
+      title: "pancakes",
+      img: "pancakes.png",
+      url: "https://pancakes.gay/",
+    },
+    {
+      title: "natty",
+      img: "natty.png",
+      url: "https://natty.gay/",
+    },
+    {
+      title: "N",
+      img: "n.png",
+      url: "https://n.cyrneko.eu/",
+    },
+    {
+      title: "Aman",
+      img: "zeffo.gif",
+      url: "https://zeffo.me/",
+    },
+    {
+      title: "the human finny",
+      img: "humanfinny.jpg",
+      url: "https://humanfinny.neocities.org",
+    },
+    {
+      title: "HEROTRAP",
+      img: "herotrap.png",
+      url: "https://herotrap.neocities.org/"
+    },
+
+    {
+      title: "Selvan",
+      img: "selvan.png",
+      url: "https://the-selvan.neocities.org/"
+    },
+    {
+      title: "makoenergy",
+      img: "makoenergy.gif",
+      url: "https://makoenergy.neocities.org/"
+    },
+    {
+      title: "Andrei",
+      img: "andrei.gif",
+      url: "andrei.xyz"
+    }
+  ],
+  special: [
+    {
+      title: "OpenBooks",
+      img: "friends/openbooks.gif",
+      url: "https://openbooks.neocities.org",
+    },
+    {
+      title: "Trans rights now",
+      img: "general/transnow.gif",
+    },
+    {
+      title: "Bring Back the Headphone Jack",
+      img: "general/bring-back-the-headphone-jack.gif",
+    },
+    {
+      title: "Made with CSS Grid",
+      img: "general/css-grid.gif",
+    },
+    {
+      title: "Catppuccin",
+      img: "general/catppuccin.gif",
+      url: "https://catppuccin.com/",
+    },
+  ],
+};
